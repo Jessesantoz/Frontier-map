bbd94b7abf54145d37342d4fbf59dfb1d2a74b33
Jessé Santoz <108159237+Jessesantoz@users.noreply.github.com>
2022-09-27 20:08:49 -0300

    Aplicação inicial Ejs
    Correção do erro de rota;
    Criação do arquivo "html" em formato "ejs";
    Utilização de imagem da Public com a correção da rota.

--- routes.js
+++ routes.js
@@ -2,9 +2,12 @@
 function routes(app) {
 
     app.get('/', function(req, res) {
-        res.render(__dirname + '/index.html')
+        res.render(__dirname + '/vews/mapa')
     })
     
+    app.get('/frontier', function(req, res) {
+       res.render(__dirname + '/view/frontier') 
+    })
 
 }
 
